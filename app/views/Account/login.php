ae6fb9c44e2eafbae9c4003a713deecac2c1c213
Eric Xu <shiqingxuvanier@gmail.com>
2021-11-15 11:15:45 -0500

    added Account views

---
+++ app/views/Account/login.php
@@ -0,0 +1,16 @@
+<html>
+<head><title>Login</title></head><body>
+<?php
+	if($data != null){
+		echo $data;
+	}
+?>
+<a href="<?=BASE?>/User/register">Create a new User</a><br>
+Login
+<form action='' method='post'>
+	Username: <input type='text' name='username' /><br>
+	Password: <input type='password' name='password' /><br>
+	<input type='submit' name='action' value='Login' />
+</form>
+
+</body></html>
---
+++ app/views/Account/register.php
@@ -0,0 +1,12 @@
+<html>
+<head><title>Register</title></head><body>
+<a href="<?=BASE?>/User/login">return</a><br>
+Register a new user
+<form action='' method='post'>
+	Username: <input type='text' name='username' /><br>
+	Password: <input type='password' name='password' /><br>
+	Password confirmation: <input type='password' name='password_confirm' /><br>
+	<input type='submit' name='action' value='Register' />
+</form>
+
+</body></html>
