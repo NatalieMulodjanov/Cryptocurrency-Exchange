d62295a7f924f98c930b6755c47518d819febb63
NatalieMulodjanov <ntaliemulodjanov@gmail.com>
2021-11-15 12:27:44 -0500

    fixed user model because changes in db

--- app/models/User.php
+++ app/models/User.php
@@ -4,13 +4,13 @@ namespace app\models;
 
 class User extends \app\core\Model{
     public $user_id;
-    public $email;
     public $password_hash;
-    public $refferal_code;
     public $first_name;
     public $last_name;
     public $dob; 
-    public $total_funds;
+    public $two_factor_authentication_token;
+    public $email;
+    public $isAdmin;
 
     public function __construct(){
         parent::__construct();
