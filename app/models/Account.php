eba024741d06e748c8502c5c4bfa9b49be9c77a3
NatalieMulodjanov <ntaliemulodjanov@gmail.com>
2021-11-16 00:18:28 -0500

    add funds, remove funds, and get account by id methods in account model

--- app/models/Account.php
+++ app/models/Account.php
@@ -10,4 +10,27 @@ class Account extends \app\core\Model{
     public function __construct(){
         parent::__construct();
     }
+
+      // add funds to wallet
+      public function addFunds($amount){
+        $SQL = "UPDATE TABLE account SET total_funds_CAD = :total_funds_CAD + :amount WHERE account_id = :account_id";
+        $STMT = self::$_connection->prepare($SQL);
+        $STMT->execute(['amount' => $amount, 'account_id' => $this->account_id]);
+    }
+
+    // remove funds from wallet
+    public function removeFunds($amount){
+        $SQL = "UPDATE TABLE account SET total_funds_CAD -= :amount";
+        $STMT = self::$_connection->prepare($SQL);
+        $STMT->execute(['amount' => $amount]);
+    }
+
+    //get account by account id
+    public function getAccountById($account_id){
+        $SQL = "SELECT * FROM account WHERE account_id = :account_id";
+        $STMT = self::$_connection->prepare($SQL);
+        $STMT->execute(['account_id' => $account_id]);
+        $STMT->setFetchMode(\PDO::FETCH_CLASS, 'app\models\Account');
+        return $STMT->fetch();
+    }
 }
