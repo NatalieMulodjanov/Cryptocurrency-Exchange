a67538db4ed23059d66ffed349cd185f308c5a88
NatalieMulodjanov <ntaliemulodjanov@gmail.com>
2021-11-16 00:17:39 -0500

    add funds to account controller

---
+++ app/controllers/Account.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace app\controllers;
+
+class Account extends \app\core\Controller
+{
+    public function addFunds()
+    {
+        if (isset($_POST['action'])){
+            $amount = $_POST['amount'];
+            $account = new \app\models\Account();
+            $account = $account->getAccountById($_SESSION['account_id']);
+            $account->addFunds($amount);
+        } else {
+            $_SESSION['account'] = 1;
+            $this->view('Account/addFunds');
+        }
+        
+    }
+}
